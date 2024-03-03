6e3b4658c128933dcef09bc9e42a7df90984c396
estevesryan <ryanplay71@gmail.com>
2024-03-03 03:37:19 -0300

    :sparkles:feat: Skeletons pages loading

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from '@/app/components/skeletons'
+
 export default function HomeLoading() {
-  return <div>Loading...</div>
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[850px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
+  )
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
   const featuredProducts = await data.products.filter(
     (product) => product.featured,
   )
