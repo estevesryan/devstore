daa09ae6213a2c8a24a62af53156983d2c57ec68
estevesryan <ryanplay71@gmail.com>
2024-03-02 22:43:05 -0300

    feat: :sparkles: fechting products from api

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,16 +1,30 @@
+import { api } from '@/app/data/api'
+import { Product } from '@/app/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
 
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+
+  const products = response.json()
+
+  return products
+}
+
 export default async function Home() {
+  const [highLightedProduct, ...othersProducts] = await getFeaturedProducts()
+
+  console.log(othersProducts)
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highLightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-never-stop-learning.png"
+          src={highLightedProduct.image}
           width={920}
           height={920}
           quality={80}
@@ -18,54 +32,48 @@ export default async function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom never stop learning</span>
+          <span className="text-sm truncate">{highLightedProduct.title}</span>
           <span className="flex h-full items-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {highLightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
 
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-java.png"
-          width={920}
-          height={920}
-          quality={80}
-          alt="Moletom"
-        />
+      {othersProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/products/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              className="group-hover:scale-105 transition-transform duration-500"
+              src={product.image}
+              width={920}
+              height={920}
+              quality={80}
+              alt="Moletom"
+            />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom never stop learning</span>
-          <span className="flex h-full items-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div>
-      </Link>
-
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/camiseta-dowhile-2022.png"
-          width={920}
-          height={920}
-          quality={80}
-          alt="Moletom"
-        />
-
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom never stop learning</span>
-          <span className="flex h-full items-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div>
-      </Link>
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/app/data/api.ts
+++ src/app/data/api.ts
@@ -2,7 +2,8 @@ import { env } from '@/app/env'
 
 export async function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseUrl)
+  const apiPrefix = '/api'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
 
   return await fetch(url, init)
 }
