9c2c5854af565c3ac1363f9ab129b45e058bc22f
estevesryan <ryanplay71@gmail.com>
2024-03-03 02:47:59 -0300

    :sparkles: feat: caching data

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
 
   const products = response.json()
 
@@ -13,8 +17,6 @@ async function getFeaturedProducts(): Promise<Product[]> {
 
 export default async function Home() {
   const [highLightedProduct, ...othersProducts] = await getFeaturedProducts()
-
-  console.log(othersProducts)
 
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
