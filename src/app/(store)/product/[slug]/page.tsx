c7677e9ec78b7d0a70c083ab30a1c76a8e90ed45
estevesryan <ryanplay71@gmail.com>
2024-03-03 07:09:59 -0300

    feat: :sparkles: generating statics pages on build

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -21,6 +21,17 @@ async function getProduct(slug: string): Promise<Product> {
   return product
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return {
+      slug: product.slug,
+    }
+  })
+}
+
 export async function generateMetadata({
   params,
 }: ProductProps): Promise<Metadata> {
