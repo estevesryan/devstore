40af380e99b5abb0d81ad65e83d8a4ab8a94d556
estevesryan <ryanplay71@gmail.com>
2024-03-03 21:49:16 -0300

    :sparkles: feat: implements addToCart function

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AddToCartButton from '@/app/components/add-to-cart-button'
 import { api } from '@/app/data/api'
 import { Product } from '@/app/data/types/product'
 import { Metadata } from 'next'
@@ -112,12 +113,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import { Search, ShoppingBag } from 'lucide-react'
+import { Search } from 'lucide-react'
 import Image from 'next/image'
+import CartWidget from './cart-widget'
 
 export default function Header() {
   return (
@@ -19,10 +20,7 @@ export default function Header() {
         </form>
       </div>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart (0)</span>
-        </div>
+        <CartWidget />
 
         <div className="w-px h-4 bg-zinc-700" />
 
